637d1420a5875ac4e2339f4b52fef399150aabf8
PatricioCristaldo <patricio_cristaldo@outlook.com>
2022-07-06 00:53:38 -0300

    actualizacion

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-/* const express = require("express");
-const path = require("path");
-const app = express();
-
-const publicPath = path.resolve(__dirname, "./public") ;
-app.use( express.static(publicPath));
-
-app.listen(3000, () => {
-    console.log("servidor corriendo en el puerto 3000");
-});
-
-app.get("/", (req,res) => { res.sendFile("/")}) */
 
 const express = require('express');
 const path = require('path')
